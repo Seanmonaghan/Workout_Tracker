948de9a4937c5de92aa42c0f19f471d046761372
Sean Monaghan <smonagha@conncoll.edu>
2021-05-17 23:50:13 -0400

    fixed server routes to get the homepage to display

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -2,16 +2,16 @@ const path = require('path');
 
 module.exports = (app) => {
 
-  app.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+  app.get('/exercise', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'exercise.html'));
   });
 
-  app.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/stats.html'));
+  app.get('/stats', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'stats.html'));
   });
 
   // If no matching route is found default to home
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+  app.get('*', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
   });
 };
